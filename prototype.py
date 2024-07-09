48ff2c1522381ef0cf39b1369b9b6e2c241cf90c
yu-hao123 <yuhaowangxia@gmail.com>
2024-07-09 19:11:36 -0300

    feat: add inspiration marks visualization

--- prototype.py
+++ prototype.py
@@ -1,9 +1,12 @@
+import os
 import sys
 import csv
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
 import pyqtgraph as pg
 import numpy as np
 from utils import retrieve_parity_marks
+
+os.environ["QT_ENABLE_HIGHDPI_SCALING"] = "1"
 
 # Set background to white
 pg.setConfigOption('background', 'w')
@@ -16,34 +19,44 @@ class PlotWindow(QtWidgets.QWidget):
 
         # Load data
         self.time, self.flow, self.pressure, self.volume, self.pmus, self.ref = self.load_data('coletapcv_adequate.csv')
+        self.ins_marks, self.exp_marks = retrieve_parity_marks(self.volume * 10)
 
-        # Create a layout
-        layout = QtWidgets.QVBoxLayout()
+        plot_layout = QtWidgets.QVBoxLayout()
 
-        # Create plots
-        self.plot1 = pg.PlotWidget(title="Flow")
-        self.plot2 = pg.PlotWidget(title="Pressure")
+        self.plot1 = pg.PlotWidget(title="Pressure")
+        self.plot2 = pg.PlotWidget(title="Flow")
         self.plot3 = pg.PlotWidget(title="Volume")
 
-        # Show grids
         self.plot1.showGrid(x=True, y=True)
         self.plot2.showGrid(x=True, y=True)
         self.plot3.showGrid(x=True, y=True)
 
-         # Link the views
         self.plot2.setXLink(self.plot1)
         self.plot3.setXLink(self.plot1)
 
-        # Add plots to layout
-        layout.addWidget(self.plot1)
-        layout.addWidget(self.plot2)
-        layout.addWidget(self.plot3)
+        plot_layout.addWidget(self.plot1)
+        plot_layout.addWidget(self.plot2)
+        plot_layout.addWidget(self.plot3)
 
-        # Set layout
-        self.setLayout(layout)
+        button_layout = QtWidgets.QVBoxLayout()
+        self.zoom_in_button = QtWidgets.QPushButton("Zoom In")
+        self.zoom_out_button = QtWidgets.QPushButton("Zoom Out")
+        self.smooth_button = QtWidgets.QPushButton("Smooth")
 
-        # Plot data
+        button_layout.addWidget(self.zoom_in_button)
+        button_layout.addWidget(self.zoom_out_button)
+        button_layout.addWidget(self.smooth_button)
+        button_layout.addStretch()  # Add stretch to push buttons to the top
+
+        # horizontal layout to contain both plot layout and button layout
+        main_layout = QtWidgets.QHBoxLayout()
+        main_layout.addLayout(plot_layout)
+        main_layout.addLayout(button_layout)
+
+        self.setLayout(main_layout)
+
         self.plot_data()
+        self.add_marks()
 
     def load_data(self, filename):
         time = []
@@ -66,13 +79,28 @@ class PlotWindow(QtWidgets.QWidget):
         return np.array(time), np.array(flow), np.array(pressure), np.array(volume), np.array(pmus), np.array(ref)
 
     def plot_data(self):
-        self.plot1.plot(self.time, self.flow, pen=pg.mkPen('k', width=2))
-        self.plot2.plot(self.time, self.pressure, pen=pg.mkPen('k', width=2))
-        self.plot3.plot(self.time, self.volume, pen=pg.mkPen('k', width=2))
+        self.plot1.plot(self.time, self.pressure, pen=pg.mkPen('b', width=2))
+        self.plot2.plot(self.time, self.flow, pen=pg.mkPen('b', width=2))
+        self.plot3.plot(self.time, self.volume, pen=pg.mkPen('b', width=2))
 
+    def add_marks(self):
+        # Add vertical lines for ins_marks and exp_marks on Volume plot
+        for mark in self.ins_marks:
+            line = pg.InfiniteLine(pos=self.time[mark], angle=90, movable=False, pen=pg.mkPen('r', width=2, style=pg.QtCore.Qt.DotLine))
+            self.plot2.addItem(line)
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Waveform Viewer")
+        self.setGeometry(100, 100, 1600, 900)
+
+        self.plot_window = PlotWindow()
+        self.setCentralWidget(self.plot_window)
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
-    main = PlotWindow()
+    app.setHighDpiScaleFactorRoundingPolicy(QtCore.Qt.HighDpiScaleFactorRoundingPolicy.PassThrough)
+    main = MainWindow()
     main.show()
     sys.exit(app.exec_())
